90d1e3e7eb2442e062889b423d8e05ab9b6853f3
Unknown <anthony.marechal@yahoo.fr>
2018-12-05 16:39:30 +0100

    :construction: WIP slicing source code

--- app.py
+++ app.py
@@ -1,178 +1,39 @@
 #!/usr/bin/env python3
 # _*_ coding: utf8 _*_
 #
-# Foobar.py: Description of what foobar does.
+# app.py: play the game
 # Sources :
 #           Initial code without AI : https://www.ilemaths.net/sujet-taquin-en-python-771297.html
 #
 
-__author__ = "Anthony MARECHAL"
+__author__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
 __copyright__ = "Copyright 2018, FuzeProg"
-__credits__ = ["Anthony MARECHAL"]
+__credits__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
 __version__ = "1.0.1"
-__maintainer__ = "Anthony MARECHAL"
-__email__ = "anthony.marechal@etu.uphf.fr"
+__maintainer__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__email__ = ["anthony.marechal@etu.uphf.fr", "ombeline.mozdzierz@etu.uphf.fr"]
 __status__ = "In product"
 
-# *******************************************************************************
-# IMPORT ***********************************************************************
-# *******************************************************************************
-
-# module de generation de nombres aleatoires
+from taquin import *
 import random
 
 
 
-# *******************************************************************************
-# DEF **************************************************************************
-# *******************************************************************************
-
-# -- afficher_taquin ----------------------------------------------------------
-#
-# Description  : affichage du Taquin
-#
-# Parameters   :
-#
-# Return       :
-#
-# Notes        :
-#                   --- --- ---
-#                  | 0 | 1 | 2 |
-#                   --- --- ---
-#                  | 3 | 4 | 5 |
-#                   --- --- ---
-#                  | 6 | 7 | 8 |
-#                   --- --- ---
-#
-def afficher_taquin():
-    print("   --- --- ---")
-    print("  |", l_case[0], "|", l_case[1], "|", l_case[2], "|")
-    print("   --- --- ---")
-    print("  |", l_case[3], "|", l_case[4], "|", l_case[5], "|")
-    print("   --- --- ---")
-    print("  |", l_case[6], "|", l_case[7], "|", l_case[8], "|")
-    print("   --- --- ---")
-    print(" ")
-
-
-# -- echanger_cases ----------------------------------------------------------
-#
-# Description  : echange de 2 cases
-#
-# Parameters   :
-#
-# Return       :
-#
-# Notes        : aucunes cases echangées si pas possible
-#
-def echanger_cases(case1, case2):
-    # echanges horizontaux
-
-    # 0 <-> 1 ?
-    if (case1 == 0 and case2 == 1) or (case1 == 1 and case2 == 0):
-
-        l_case[0], l_case[1] = l_case[1], l_case[0]
-
-    # 1 <-> 2 ?
-    elif (case1 == 1 and case2 == 2) or (case1 == 2 and case2 == 1):
-
-        l_case[1], l_case[2] = l_case[2], l_case[1]
-
-    # 3 <-> 4 ?
-    elif (case1 == 3 and case2 == 4) or (case1 == 4 and case2 == 3):
-
-        l_case[3], l_case[4] = l_case[4], l_case[3]
-
-    # 4 <-> 5 ?
-    elif (case1 == 4 and case2 == 5) or (case1 == 5 and case2 == 4):
-
-        l_case[4], l_case[5] = l_case[5], l_case[4]
-
-    # 6 <-> 7 ?
-    elif (case1 == 6 and case2 == 7) or (case1 == 7 and case2 == 6):
-
-        l_case[6], l_case[7] = l_case[7], l_case[6]
-
-    # 7 <-> 8
-    elif (case1 == 7 and case2 == 8) or (case1 == 8 and case2 == 7):
-
-        l_case[7], l_case[8] = l_case[8], l_case[7]
-
-    # echanges verticaux
-
-    # 0 <-> 3 ?
-    elif (case1 == 0 and case2 == 3) or (case1 == 3 and case2 == 0):
-
-        l_case[0], l_case[3] = l_case[3], l_case[0]
-
-    # 3 <-> 6 ?
-    elif (case1 == 3 and case2 == 6) or (case1 == 6 and case2 == 3):
-
-        l_case[3], l_case[6] = l_case[6], l_case[3]
-
-    # 1 <-> 4 ?
-    elif (case1 == 1 and case2 == 4) or (case1 == 4 and case2 == 1):
-
-        l_case[1], l_case[4] = l_case[4], l_case[1]
-
-    # 4 <-> 7 ?
-    elif (case1 == 4 and case2 == 7) or (case1 == 7 and case2 == 4):
-
-        l_case[4], l_case[7] = l_case[7], l_case[4]
-
-    # 2 <-> 5 ?
-    elif (case1 == 2 and case2 == 5) or (case1 == 5 and case2 == 2):
-
-        l_case[2], l_case[5] = l_case[5], l_case[2]
-
-    # 5 <-> 8
-    elif (case1 == 5 and case2 == 8) or (case1 == 8 and case2 == 5):
-
-        l_case[5], l_case[8] = l_case[8], l_case[5]
-
-
-# *******************************************************************************
-# MAIN *************************************************************************
-# *******************************************************************************
-
-
-
-# initialisation du Taquin ----------------------------------------------------
-
-# liste vide par defaut
 l_case = []
-
 i = 0
 
 while i < 9:
-
-    # chiffre aleatoire de 0 a 8
     chiffre = random.randrange(0, 9)
 
-    # deja eu ?
     if chiffre not in l_case:
-        # non : on le garde
         l_case.append(chiffre)
-
         i += 1
 
-# jeu -------------------------------------------------------------------------
-
 while (True):
-
-    # affichage du Taquin
-    afficher_taquin()
-
-    # saisie
+    afficher_taquin(l_case)
     numero = int(input(" Entrez le numero de la piece a bouger de 1 a 8 : "))
 
-    # numero valide ?
     if 1 <= numero and numero <= 8:
-        # oui : recherche de la case contenant le zero
         case_zero = l_case.index(0)
-
-        # recherche de la case contenant le numero
         case_numero = l_case.index(numero)
-
-        # echange (si possible)
-        echanger_cases(case_zero, case_numero)
+        echanger_cases(case_zero, case_numero, l_case)
---
+++ taquin.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+#
+# taquin.py: display the game
+# Sources :
+#           Initial code without AI : https://www.ilemaths.net/sujet-taquin-en-python-771297.html
+#
+
+__author__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__copyright__ = "Copyright 2018, FuzeProg"
+__credits__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__version__ = "1.0.1"
+__maintainer__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__email__ = ["anthony.marechal@etu.uphf.fr", "ombeline.mozdzierz@etu.uphf.fr"]
+__status__ = "In product"
+
+def afficher_taquin(l_case):
+    print("   --- --- ---")
+    print("  |", l_case[0], "|", l_case[1], "|", l_case[2], "|")
+    print("   --- --- ---")
+    print("  |", l_case[3], "|", l_case[4], "|", l_case[5], "|")
+    print("   --- --- ---")
+    print("  |", l_case[6], "|", l_case[7], "|", l_case[8], "|")
+    print("   --- --- ---")
+    print(" ")
+
+def echanger_cases(case1, case2, l_case):
+
+    if (case1 == 0 and case2 == 1) or (case1 == 1 and case2 == 0):
+
+        l_case[0], l_case[1] = l_case[1], l_case[0]
+
+    elif (case1 == 1 and case2 == 2) or (case1 == 2 and case2 == 1):
+
+        l_case[1], l_case[2] = l_case[2], l_case[1]
+
+    elif (case1 == 3 and case2 == 4) or (case1 == 4 and case2 == 3):
+
+        l_case[3], l_case[4] = l_case[4], l_case[3]
+
+    elif (case1 == 4 and case2 == 5) or (case1 == 5 and case2 == 4):
+
+        l_case[4], l_case[5] = l_case[5], l_case[4]
+
+    elif (case1 == 6 and case2 == 7) or (case1 == 7 and case2 == 6):
+
+        l_case[6], l_case[7] = l_case[7], l_case[6]
+
+    elif (case1 == 7 and case2 == 8) or (case1 == 8 and case2 == 7):
+
+        l_case[7], l_case[8] = l_case[8], l_case[7]
+
+    elif (case1 == 0 and case2 == 3) or (case1 == 3 and case2 == 0):
+
+        l_case[0], l_case[3] = l_case[3], l_case[0]
+
+    elif (case1 == 3 and case2 == 6) or (case1 == 6 and case2 == 3):
+
+        l_case[3], l_case[6] = l_case[6], l_case[3]
+
+    elif (case1 == 1 and case2 == 4) or (case1 == 4 and case2 == 1):
+
+        l_case[1], l_case[4] = l_case[4], l_case[1]
+
+    elif (case1 == 4 and case2 == 7) or (case1 == 7 and case2 == 4):
+
+        l_case[4], l_case[7] = l_case[7], l_case[4]
+
+    elif (case1 == 2 and case2 == 5) or (case1 == 5 and case2 == 2):
+
+        l_case[2], l_case[5] = l_case[5], l_case[2]
+
+    elif (case1 == 5 and case2 == 8) or (case1 == 8 and case2 == 5):
+
+        l_case[5], l_case[8] = l_case[8], l_case[5]
